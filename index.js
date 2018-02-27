3aa6354050c476cf754c4cbb2ffbb2566e1faccc
Pierre <pierre@dash.org>
2018-02-27 10:26:41 +0200

    fn rename + some lint fixes

--- index.js
+++ index.js
@@ -1,81 +1,77 @@
-'use strict';
 
-var u256 = require("./lib/u256");
+
+const u256 = require('./lib/u256');
 /**
 * @param {Array} blocks - An array of blocks having height, target, imestamp property
 * @params {Int} [blockTime=150] - A block time value
 * @return {Int} compact - The difficulty value
 */
-module.exports.darkGravityWaveTargetWithBlocks = function(blocks, blockTime) {
-    /* current difficulty formula, dash - based on DarkGravity v3, original work done by evan duffield, modified for javascript */
-    blocks = blocks.slice();
-    var previousBlock = blocks.pop();
+module.exports.getTarget = function getTarget(blocks, blockTime) {
+  /* current difficulty formula, dash - based on DarkGravity v3, original work done by evan duffield, modified for javascript */
+  blocks = blocks.slice();
+  const previousBlock = blocks.pop();
 
-    if (!blockTime) blockTime = 150; //Dash default in second (2.5 min)
-    var nActualTimespan = 0;
-    var lastBlockTime = 0;
-    var blockCount = 0;
-    var sumTargets = new u256();
+  if (!blockTime) blockTime = 150; // Dash default in second (2.5 min)
+  let nActualTimespan = 0;
+  let lastBlockTime = 0;
+  let blockCount = 0;
+  let sumTargets = new u256();
 
-    if (previousBlock.height == 0 || previousBlock.height < 24) {
-        // This is the first block or the height is < PastBlocksMin
-        // Return minimal required work. (1e0ffff0)
-        return 0x1e0ffff0;
+  if (previousBlock.height == 0 || previousBlock.height < 24) {
+    // This is the first block or the height is < PastBlocksMin
+    // Return minimal required work. (1e0ffff0)
+    return 0x1e0ffff0;
+  }
+
+  let currentBlock = previousBlock;
+  // loop over the past n blocks, where n == PastBlocksMax
+  for (blockCount = 1; currentBlock && currentBlock.height > 0 && blockCount <= 24; blockCount++) {
+    // Calculate average difficulty based on the blocks we iterate over in this for loop
+    if (blockCount <= 24) {
+      const currentTarget = new u256();
+      currentTarget.setCompact(currentBlock.target);
+      if (blockCount === 1) {
+        sumTargets = currentTarget.plus(currentTarget);
+      } else {
+        sumTargets.add(currentTarget);
+      }
     }
 
-    var currentBlock = previousBlock;
-    // loop over the past n blocks, where n == PastBlocksMax
-    for (blockCount = 1; currentBlock && currentBlock.height > 0 && blockCount <= 24; blockCount++) {
-
-        // Calculate average difficulty based on the blocks we iterate over in this for loop
-        if (blockCount <= 24) {
-            var currentTarget = new u256();
-            currentTarget.setCompact(currentBlock.target);
-            if (blockCount === 1) {
-                sumTargets = currentTarget.plus(currentTarget);
-            }
-            else {
-                sumTargets.add(currentTarget);
-            }
-        }
-
-        // If this is the second iteration (LastBlockTime was set)
-        if (lastBlockTime > 0) {
-            // Calculate time difference between previous block and current block
-            var currentBlockTime = currentBlock.timestamp;
-            var diff = ((lastBlockTime) - (currentBlockTime));
-            // Increment the actual timespan
-            nActualTimespan += diff;
-        }
-        // Set lastBlockTime to the block time for the block in current iteration
-        lastBlockTime = currentBlock.timestamp;
-
-        currentBlock = blocks.pop();
+    // If this is the second iteration (LastBlockTime was set)
+    if (lastBlockTime > 0) {
+      // Calculate time difference between previous block and current block
+      const currentBlockTime = currentBlock.timestamp;
+      const diff = ((lastBlockTime) - (currentBlockTime));
+      // Increment the actual timespan
+      nActualTimespan += diff;
     }
-    // darkTarget is the difficulty
-    var darkTarget = sumTargets.divide(blockCount);
+    // Set lastBlockTime to the block time for the block in current iteration
+    lastBlockTime = currentBlock.timestamp;
 
-    // nTargetTimespan is the time that the CountBlocks should have taken to be generated.
-    var nTargetTimespan = (blockCount - 1) * blockTime;
-    // Limit the re-adjustment to 3x or 0.33x
-    // We don't want to increase/decrease diff too much.
-    if (nActualTimespan < nTargetTimespan / 3.0)
-        nActualTimespan = nTargetTimespan / 3.0;
-    if (nActualTimespan > nTargetTimespan * 3.0)
-        nActualTimespan = nTargetTimespan * 3.0;
+    currentBlock = blocks.pop();
+  }
+  // darkTarget is the difficulty
+  let darkTarget = sumTargets.divide(blockCount);
 
-    // Calculate the new difficulty based on actual and target timespan.
-    var wew = darkTarget.multiplyWithInteger(Math.floor(nActualTimespan));
-    var aas = wew.divide(nTargetTimespan);
-    darkTarget = darkTarget.multiplyWithInteger(Math.floor(nActualTimespan)).divide(nTargetTimespan);
+  // nTargetTimespan is the time that the CountBlocks should have taken to be generated.
+  const nTargetTimespan = (blockCount - 1) * blockTime;
+  // Limit the re-adjustment to 3x or 0.33x
+  // We don't want to increase/decrease diff too much.
+  if (nActualTimespan < nTargetTimespan / 3.0) { nActualTimespan = nTargetTimespan / 3.0; }
+  if (nActualTimespan > nTargetTimespan * 3.0) { nActualTimespan = nTargetTimespan * 3.0; }
 
-    var compact = darkTarget.getCompact();
+  // Calculate the new difficulty based on actual and target timespan.
+  const wew = darkTarget.multiplyWithInteger(Math.floor(nActualTimespan));
+  const aas = wew.divide(nTargetTimespan);
+  darkTarget = darkTarget.multiplyWithInteger(Math.floor(nActualTimespan)).divide(nTargetTimespan);
 
-    // If calculated difficulty is lower than the minimal diff, set the new difficulty to be the minimal diff.
-    if ((compact >>> 1) > 0xF07FFF8) {
-        compact = 0x1e0ffff0;
-    }
+  let compact = darkTarget.getCompact();
 
-    // Return the new diff.
-    return compact;
-}
+  // If calculated difficulty is lower than the minimal diff, set the new difficulty to be the minimal diff.
+  if ((compact >>> 1) > 0xF07FFF8) {
+    compact = 0x1e0ffff0;
+  }
+
+  // Return the new diff.
+  return compact;
+};
