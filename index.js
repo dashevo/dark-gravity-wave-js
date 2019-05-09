ff94209117bcd7ac67111547b7fe9a6f87befb5c
Cofresi <cofresi@dash.org>
2019-05-09 00:58:23 -0400

    adding network param

--- index.js
+++ index.js
@@ -4,22 +4,41 @@ const getDoubleFrom256 = require('./lib/utils/getDoubleFrom256');
 
 const maxBlocks = 24;
 
-// Using regtest targets according to Bitcoin specifications
-// Todo: adapt to allow for test/mainnet targets (0x1d00ffff)
-const maxTarget = 0x207fffff;
+const maxTargetMainnet = 0x1e0ffff0;
+const maxTargetRegtest = 0x207fffff;
 
 function getDarkTarget(blocks) {
   return blocks.reduce((sum, b) => sum + getDoubleFrom256(b.target), 0.0) / (blocks.length + 1);
 }
 
+const getMaxTarget = (network) => {
+  switch (network) {
+    case 'mainnet':
+      return maxTargetMainnet;
+    case 'livenet':
+      return maxTargetMainnet;
+    case 'testnet':
+      return maxTargetMainnet;
+    case 'devnet':
+      return maxTargetRegtest;
+    case 'regtest':
+      return maxTargetRegtest;
+    default:
+      return maxTargetMainnet;
+  }
+};
+
 /**
 * @param {Array} allHeaders - An array of blocks having target and timestamp property
 * @param {int} blockTime - A block time value
+* @param {string} [network='mainnet'] - A block time value
 * @return {int} compact - The difficulty value
 * current difficulty formula, dash - based on DarkGravity v3
 * original work done by Evan Duffield, modified for javascript
 */
-function getTarget(allHeaders, blockTime) {
+function getTarget(allHeaders, blockTime, network = 'mainnet') {
+  const maxTarget = getMaxTarget(network);
+
   if (allHeaders.length < maxBlocks) return getDoubleFrom256(maxTarget);
 
   const blocks = allHeaders.slice(Math.max(allHeaders.length - maxBlocks, 0)).reverse();
@@ -37,8 +56,8 @@ function getTarget(allHeaders, blockTime) {
   return Math.min(darkTarget, maxTargetAsDouble);
 }
 
-function isValidTarget(headerBits, allHeaders, blockTime = 150) {
-  return getDoubleFrom256(headerBits) <= getTarget(allHeaders, blockTime);
+function isValidTarget(headerBits, allHeaders, network = 'mainnet', blockTime = 150) {
+  return getDoubleFrom256(headerBits) <= getTarget(allHeaders, blockTime, network);
 }
 
 module.exports = {
