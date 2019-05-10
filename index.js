276cc4f81dbc2376d957bb240f853a925ce59dd4
Cofresi <cofresi@dash.org>
2019-05-09 22:16:14 -0400

    replace isValidTarget with hasValidTarget

--- index.js
+++ index.js
@@ -7,43 +7,64 @@ const maxBlocks = 24;
 const maxTargetMainnet = 0x1e0ffff0;
 const maxTargetRegtest = 0x207fffff;
 
+// Dash: 2.5 minutes
+const powTargetSpacing = 2.5 * 60;
+
 function getDarkTarget(blocks) {
   return blocks.reduce((sum, b) => sum + getDoubleFrom256(b.target), 0.0) / (blocks.length + 1);
 }
 
-const getMaxTarget = (network) => {
+const getNetworkParams = (network) => {
   switch (network) {
     case 'mainnet':
-      return maxTargetMainnet;
+      return { maxTargetMainnet, allowMinDifficultyBlocks: false };
     case 'livenet':
-      return maxTargetMainnet;
+      return { maxTargetMainnet, allowMinDifficultyBlocks: false };
     case 'testnet':
-      return maxTargetMainnet;
+      return { maxTargetMainnet, allowMinDifficultyBlocks: true };
     case 'devnet':
-      return maxTargetRegtest;
+      return { maxTargetRegtest, allowMinDifficultyBlocks: true };
     case 'regtest':
-      return maxTargetRegtest;
+      return { maxTargetRegtest, allowMinDifficultyBlocks: true };
     default:
-      return maxTargetMainnet;
+      return { maxTargetMainnet, allowMinDifficultyBlocks: false };
   }
 };
 
 /**
 * @param {Array} allHeaders - An array of blocks having target and timestamp property
-* @param {int} blockTime - A block time value
-* @param {string} [network='mainnet'] - A block time value
-* @return {int} compact - The difficulty value
+* @param {object} newHeader - a block header
+* @param {string} [network='mainnet'] - a block time value
+* @return {int} compact - the difficulty value
 * current difficulty formula, dash - based on DarkGravity v3
+* https://github.com/dashpay/dash/blob/master/src/pow.cpp#L186-L204 and
+* https://github.com/dashpay/dash/blob/master/src/pow.cpp#L82-L145
 * original work done by Evan Duffield, modified for javascript
 */
-function getTarget(allHeaders, blockTime, network = 'mainnet') {
-  const maxTarget = getMaxTarget(network);
+function getTarget(allHeaders, newHeader, network = 'mainnet') {
+  const { maxTarget, allowMinDifficultyBlocks } = getNetworkParams(network);
 
   if (allHeaders.length < maxBlocks) return getDoubleFrom256(maxTarget);
 
   const blocks = allHeaders.slice(Math.max(allHeaders.length - maxBlocks, 0)).reverse();
 
-  const timeSpanTarget = (blocks.length) * blockTime;
+  // TODO: write tests
+  if (allowMinDifficultyBlocks) {
+    // recent block is more than 2 hours old
+    if (newHeader.timestamp > blocks[0].timestamp + (2 * 60 * 60)) {
+      return getDoubleFrom256(maxTarget);
+    }
+    // recent block is more than 10 minutes old
+    if (newHeader.timestamp > blocks[0].timestamp + (powTargetSpacing * 4)) {
+      let bnNew = getDoubleFrom256(blocks[0].target) * 10;
+      if (bnNew > maxTarget) {
+        bnNew = maxTarget;
+      }
+      return getDoubleFrom256(bnNew);
+    }
+  }
+
+  const timeSpanTarget = (blocks.length) * 150;
   const nActualTimespan = Math.min(
     Math.max(blocks[0].timestamp - blocks[blocks.length - 1].timestamp, timeSpanTarget / 3.0),
     timeSpanTarget * 3.0,
@@ -56,10 +77,14 @@ function getTarget(allHeaders, blockTime, network = 'mainnet') {
   return Math.min(darkTarget, maxTargetAsDouble);
 }
 
-function isValidTarget(headerBits, allHeaders, network = 'mainnet', blockTime = 150) {
-  return getDoubleFrom256(headerBits) <= getTarget(allHeaders, blockTime, network);
+function hasValidTarget(newHeader, previousHeaders, network = 'mainnet') {
+  if (previousHeaders.length < maxBlocks) {
+    throw new Error('previousHeaders.length needs to be at least 24');
+  }
+  return getDoubleFrom256(newHeader.target) <= getTarget(previousHeaders, newHeader, network);
 }
 
 module.exports = {
-  isValidTarget,
+  hasValidTarget,
+  getTarget,
 };
